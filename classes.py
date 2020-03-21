fab4fb4aaca6249e744a0bf2ef857af032f00e60
Maria Afanaseva <mashenkachukina@gmail.com>
2020-03-21 18:16:04 +0100

    Added tests for all classes, added new classes (Change), added pattern Facade

--- classes.py
+++ classes.py
@@ -1,69 +1,6 @@
 from copy import deepcopy
 
 
-class Person:
-
-    def __init__(self, name, surname,  e_mail, phone, address):
-        self.name = name
-        self.surname = surname
-        self.e_mail = e_mail
-        self.phone = phone
-        self.address = address
-
-    def __str__(self):
-        return f'{self.name}, {self.surname}, ' \
-            f'{self.e_mail}, {self.phone}, {self.address}'
-
-
-class Student(Person):
-
-    def __init__(self, name, surname,  e_mail, phone,
-                 address, student_number, language_level,
-                 curses):
-        super().__init__(name, surname,  e_mail, phone, address)
-        self.student_number = student_number
-        self.language_level = language_level
-        self.curses = curses
-
-    def __str__(self):
-        return f'number: {self.student_number} ' \
-            f'level: {self.language_level}, courses: {self.curses}'
-
-
-class Teacher(Person):
-
-    def __init__(self, name, surname,  e_mail, phone, address,
-                 languages, diplomas, salary):
-        super().__init__(name, surname,  e_mail, phone, address)
-        self.languages = languages
-        self.diplomas = diplomas
-        self.salary = salary
-
-    def __str__(self):
-        return f'languages: {self.languages}, ' \
-            f'diplomas: {self.diplomas}, salary: {self.salary}'
-
-
-class Course:
-
-    def __init__(self, language, level, price,
-                 teachers, students,
-                 address, books):
-        self.language = language
-        self.level = level
-        self.price = price
-        self.teachers = teachers
-        self.students = students
-        self.address = address
-        self.books = books
-
-    def __str__(self):
-        return f'{self.language}, {self.level}, ' \
-            f'price: {self.price}, teachers: {self.teachers}, ' \
-            f'students: {self.students}, address: {self.address}, ' \
-            f'books: {self.books}'
-
-
 class Address:
 
     def __init__(self):
@@ -80,7 +17,7 @@ class Address:
         for attr in self.__dict__:
             value = self.__getattribute__(attr)
             if value:
-                ret += f'{attr}: {value} '
+                ret += f'{attr}: {value}, '
         return ret
 
 
@@ -123,9 +60,52 @@ class AddressBuilder:
         return self.address
 
 
+class Person:
+
+    def __init__(self, person_id, name, surname,  e_mail, phone, address):
+        self.person_id = person_id
+        self.name = name
+        self.surname = surname
+        self.e_mail = e_mail
+        self.phone = phone
+        self.address = address
+
+    def __str__(self):
+        return f'{self.person_id}, {self.name}, {self.surname}, ' \
+            f'{self.e_mail}, {self.phone}, {self.address}'
+
+
+class Student(Person):
+
+    def __init__(self, person_id, name, surname,  e_mail, phone,
+                 address, language_level,
+                 courses_id):
+        super().__init__(person_id, name, surname,  e_mail, phone, address)
+        self.language_level = language_level
+        self.courses_id = courses_id
+
+    def __str__(self):
+        return f'level: {self.language_level}, courses_id: {self.courses_id}'
+
+
+class Teacher(Person):
+
+    def __init__(self, person_id, name, surname,  e_mail, phone, address,
+                 languages, courses_id, diplomas, salary):
+        super().__init__(person_id, name, surname,  e_mail, phone, address)
+        self.languages = languages
+        self.courses_id = courses_id
+        self.diplomas = diplomas
+        self.salary = salary
+
+    def __str__(self):
+        return f'languages: {self.languages}, courses_id: {self.courses_id}, ' \
+            f'diplomas: {self.diplomas}, salary: {self.salary}'
+
+
 class PersonFactory:
 
-    """ Pattern FATORY METHOD """
+    """ Pattern FACTORY METHOD """
 
     person_types = {
         'teacher': Teacher,
@@ -133,27 +113,145 @@ class PersonFactory:
     }
 
     @staticmethod
-    def create_person(person_type, name,
+    def create_person(person_type, person_id, name,
                       surname,  e_mail, phone, address, **kwargs):
         person_class = PersonFactory.person_types[person_type]
-        person = person_class(name=name, surname=surname,
+        person = person_class(person_id=person_id, name=name,
+                              surname=surname,
                               e_mail=e_mail, phone=phone,
                               address=address, **kwargs)
         return person
 
 
-class Change:
+class Course:
 
-    def __init__(self, item, **kwargs):
-        self.item = item
-        self.kwargs = kwargs
+    def __init__(self):
+        self.course_id = None
+        self.language = None
+        self.level = None
+        self.price = None
+        self.teachers = []
+        self.students = []
+        self.address = None
+        self.books = None
 
-    def change_data(self):
-        for attr in self.kwargs:
-            setattr(self.item, attr, self.kwargs[attr])
-        return self.item
+    def __str__(self):
+        ret = ''
+        for attr in self.__dict__:
+            value = self.__getattribute__(attr)
+            if value:
+                ret += f'{attr}: {value}, '
+        return ret
+
+
+class CourseBuilder:
+
+    """ Pattern Builder """
+
+    def __init__(self):
+        self.course = Course()
+
+    def set_course_id(self, course_id):
+        self.course.course_id = course_id
+        return self
+
+    def set_language(self, language):
+        self.course.language = language
+        return self
+
+    def set_level(self, level):
+        self.course.level = level
+        return self
+
+    def set_price(self, price):
+        self.course.price = price
+        return self
+
+    def set_teachers_id(self, teachers):
+        self.course.teachers = teachers
+        return self
+
+    def set_students_id(self, students):
+        self.course.students = students
+        return self
+
+    def set_address(self, address):
+        self.course.address = address
+        return self
+
+    def set_books(self, books):
+        self.course.books = books
+        return self
+
+    def build(self):
+        return self.course
+
+
+class ChangeCourse:
+
+    @staticmethod
+    def add_student(course, new_student_id):
+        students = course.students
+        students.append(new_student_id)
+        setattr(course, 'students', students)
+
+    @staticmethod
+    def add_teacher(course, new_teacher_id):
+        new_teachers = course.teachers
+        new_teachers.append(new_teacher_id)
+        setattr(course, 'teachers', new_teachers)
+
+
+class ChangeStudent:
+
+    @staticmethod
+    def change_name(student, new_name):
+        setattr(student, 'name', new_name)
+        return student
+
+    @staticmethod
+    def add_course(student, new_course_id):
+        new_courses = student.courses_id
+        new_courses.append(new_course_id)
+        setattr(student, 'courses_id', new_courses)
+        return student
+
+
+class ChangeTeacher:
+
+    @staticmethod
+    def change_name(teacher, new_name):
+        setattr(teacher, 'name', new_name)
+
+    @staticmethod
+    def add_course(teacher, new_course_id):
+        new_courses = teacher.courses_id
+        new_courses.append(new_course_id)
+        setattr(teacher, 'courses_id', new_courses)
+
+
+class ChangeFacade:
+
+    """Pattern Facade"""
+
+    def __init__(self):
+        self.change_student = ChangeStudent()
+        self.change_teacher = ChangeTeacher()
+        self.change_course = ChangeCourse()
+
+    def add_teacher_to_course(self, course, teacher):
+        teacher_id = teacher.person_id
+        self.change_course.add_teacher(course, teacher_id)
+        course_id = course.course_id
+        self.change_teacher.add_course(teacher, course_id)
+
+    def add_student_to_course(self, course, student):
+        student_id = student.person_id
+        self.change_course.add_student(course, student_id)
+        course_id = course.course_id
+        self.change_student.add_course(student, course_id)
 
 
 def copy_course(course):
-    """Patten prototype"""
+    """Pattern prototype"""
     return deepcopy(course)
--- tests.py
+++ tests.py
@@ -1,15 +1,379 @@
-from classes import Course, copy_course, Change, AddressBuilder
+import unittest
+from classes import (Address, AddressBuilder, Person,
+                     Student, Teacher, PersonFactory,
+                     Course, CourseBuilder,
+                     copy_course, ChangeStudent, ChangeTeacher,
+                     ChangeCourse, ChangeFacade)
+
+
+class TestAddress(unittest.TestCase):
+
+    def setUp(self):
+        self.address = Address()
+
+    def test_init(self):
+        self.assertEqual(self.address.country, None)
+        self.assertEqual(self.address.region, None)
+        self.assertEqual(self.address.city, None)
+        self.assertEqual(self.address.street, None)
+        self.assertEqual(self.address.house_number, None)
+        self.assertEqual(self.address.apartment_number, None)
+        self.assertEqual(self.address.postcode, None)
+
+    def test_str(self):
+        self.assertEqual(str(self.address), '')
+
+
+class TestAddressBuilder(unittest.TestCase):
+
+    def setUp(self):
+        self.address_builder = AddressBuilder()
+
+    def test_set_country(self):
+        self.assertEqual(self.address_builder.set_country('Germany'),
+                         self.address_builder)
+        self.assertEqual(self.address_builder.address.country, 'Germany')
+
+    def test_set_region(self):
+        self.assertEqual(self.address_builder.set_region('Bayern'),
+                         self.address_builder)
+        self.assertEqual(self.address_builder.address.region, 'Bayern')
+
+    def test_set_city(self):
+        self.assertEqual(self.address_builder.set_city('Munich'),
+                         self.address_builder)
+        self.assertEqual(self.address_builder.address.city, 'Munich')
+
+    def test_set_street(self):
+        self.assertEqual(self.address_builder.set_street('Rahel'),
+                         self.address_builder)
+        self.assertEqual(self.address_builder.address.street, 'Rahel')
+
+    def test_set_house_number(self):
+        self.assertEqual(self.address_builder.set_house_number(5),
+                         self.address_builder)
+        self.assertEqual(self.address_builder.address.house_number, 5)
+
+    def test_set_apartment_number(self):
+        self.assertEqual(self.address_builder.set_apartment_number(52),
+                         self.address_builder)
+        self.assertEqual(self.address_builder.address.apartment_number, 52)
+
+    def test_set_postcode(self):
+        self.assertEqual(self.address_builder.set_postcode(85642),
+                         self.address_builder)
+        self.assertEqual(self.address_builder.address.postcode, 85642)
+
+    def test_str(self):
+        self.assertEqual(str(self.address_builder.set_country('USA').build()),
+                         'country: USA, ')
+        self.assertEqual(str(self.address_builder.set_house_number(10).
+                             set_street('Pop').set_city('Berlin').build()),
+                         'country: USA, city: Berlin, '
+                         'street: Pop, house_number: 10, ')
+
+
+class TestPerson(unittest.TestCase):
+
+    def setUp(self):
+        self.address = AddressBuilder().set_country('Russia').build()
+        self.person = Person(1, 'Maria', 'Afanaseva',
+                             'ff@gmail.com', '454-334', self.address)
+
+    def test_init(self):
+        self.assertEqual(self.person.person_id, 1)
+        self.assertEqual(self.person.name, 'Maria')
+        self.assertEqual(self.person.surname, 'Afanaseva')
+        self.assertEqual(self.person.e_mail, 'ff@gmail.com')
+        self.assertEqual(self.person.phone, '454-334')
+        self.assertEqual(self.person.address, self.address)
+
+    def test_str(self):
+        self.assertEqual(str(self.person),
+                         '1, Maria, Afanaseva, ff@gmail.com, 454-334, '
+                         'country: Russia, ')
+
+
+class TestStudent(unittest.TestCase):
+
+    def setUp(self):
+        self.address = Address()
+        self.student = Student(2, 'Ivan', 'Ivanov',  'ivan@ivan', '9090',
+                               self.address, ['English B1'], [112])
+
+    def test_init(self):
+        self.assertEqual(self.student.person_id, 2)
+        self.assertEqual(self.student.name, 'Ivan')
+        self.assertEqual(self.student.surname, 'Ivanov')
+        self.assertEqual(self.student.e_mail, 'ivan@ivan')
+        self.assertEqual(self.student.phone, '9090')
+        self.assertEqual(self.student.address, self.address)
+        self.assertEqual(self.student.language_level, ['English B1'])
+        self.assertEqual(self.student.courses_id, [112])
+
+    def test_str(self):
+        self.assertEqual(str(self.student), "level: ['English B1'], "
+                                            "courses_id: [112]")
+
+
+class TestTeacher(unittest.TestCase):
+
+    def setUp(self):
+        self.address = Address()
+        self.teacher = Teacher(3, 'Lala', 'Fafa',  'lala@fafa.com',
+                               '7980-678', self.address,
+                               ['English', 'german'], [13],
+                               ['Manchester diploma'],
+                               5000)
+
+    def test_init(self):
+        self.assertEqual(self.teacher.person_id, 3)
+        self.assertEqual(self.teacher.name, 'Lala')
+        self.assertEqual(self.teacher.surname, 'Fafa')
+        self.assertEqual(self.teacher.e_mail, 'lala@fafa.com')
+        self.assertEqual(self.teacher.phone, '7980-678')
+        self.assertEqual(self.teacher.address, self.address)
+        self.assertEqual(self.teacher.languages, ['English', 'german'])
+        self.assertEqual(self.teacher.courses_id, [13])
+        self.assertEqual(self.teacher.diplomas, ['Manchester diploma'])
+        self.assertEqual(self.teacher.salary, 5000)
+
+    def test_str(self):
+        self.assertEqual(str(self.teacher), "languages: ['English', 'german'], courses_id: [13], "
+                                            "diplomas: ['Manchester diploma'], salary: 5000")
+
+
+class TestPersonFactory(unittest.TestCase):
+
+    def setUp(self):
+        self.address = AddressBuilder().set_city('Rom').build()
+        self.person_factory = PersonFactory()
+
+    def test_create_teacher(self):
+        teacher = self.person_factory.create_person(
+            'teacher', 4, 'Nana',
+            'Li',  'nana@h', '9009', self.address, salary=2000,
+            languages=['Italian', 'English'],
+            courses_id=[12],
+            diplomas=['Ranish diploma']
+        )
+        self.assertEqual(teacher.person_id, 4)
+        self.assertEqual(teacher.name, 'Nana')
+        self.assertEqual(teacher.surname, 'Li')
+        self.assertEqual(teacher.address, self.address)
+        self.assertIsInstance(teacher, Teacher)
+        with self.assertRaises(TypeError):
+            self.person_factory.create_person(
+                'teacher', 5, 'Nana',
+                'Li', 'nana@h', '9009', self.address, salary=2000,
+                languages=['Italian', 'English'], courses_id=[12],
+                level=['Ranish diploma']  # need diplomas not level
+            )
+
+        with self.assertRaises(KeyError):
+            self.person_factory.create_person(
+                'professor', 6, 'Nana', 'Li', 'nana@h',
+                # need teacher or student not professor
+                '9009', self.address, salary=2000,
+                languages=['Italian', 'English'],
+                diplomas=['Ranish diploma']
+            )
+
+    def test_create_student(self):
+        student = self.person_factory.create_person(
+            'student', 7, 'Mama',
+            'Li', 'mama@h', '9009', self.address,
+            language_level=['Italian A2', 'English B2'],
+            courses_id=[56]
+        )
+        self.assertEqual(student.name, 'Mama')
+        self.assertEqual(student.surname, 'Li')
+        self.assertEqual(student.address, self.address)
+        self.assertIsInstance(student, Student)
+        with self.assertRaises(TypeError):
+            self.person_factory.create_person(
+                'student', 8, 'Nana',
+                'Li', 'nana@h', '9009', self.address,
+                language_level=['Italian A2', 'English B2'],
+                diplomas=['Ranish diploma']  # need courses not diplomas
+            )
+
+
+class TestCourse(unittest.TestCase):
+
+    def setUp(self):
+        self.course = Course()
+
+    def test_init(self):
+        self.assertEqual(self.course.course_id, None)
+        self.assertEqual(self.course.language, None)
+        self.assertEqual(self.course.level, None)
+        self.assertEqual(self.course.price, None)
+        self.assertEqual(self.course.teachers, [])
+        self.assertEqual(self.course.students, [])
+        self.assertEqual(self.course.address, None)
+        self.assertEqual(self.course.books, None)
+
+    def test_str(self):
+        self.assertEqual(str(self.course), '')
+
+
+class TestCourseBuilder(unittest.TestCase):
+
+    def setUp(self):
+        self.course_builder = CourseBuilder()
+        self.address = AddressBuilder().set_city('Munich').build()
+
+    def test_set_course_id(self):
+        self.assertEqual(self.course_builder.set_course_id(123),
+                         self.course_builder)
+        self.assertEqual(self.course_builder.course.course_id, 123)
+
+    def test_set_language(self):
+        self.assertEqual(self.course_builder.set_language('german'),
+                         self.course_builder)
+        self.assertEqual(self.course_builder.course.language, 'german')
+
+    def test_set_level(self):
+        self.assertEqual(self.course_builder.set_level('B1'),
+                         self.course_builder)
+        self.assertEqual(self.course_builder.course.level, 'B1')
+
+    def test_set_price(self):
+        self.assertEqual(self.course_builder.set_price(200),
+                         self.course_builder)
+        self.assertEqual(self.course_builder.course.price, 200)
+
+    def test_set_teachers_id_id(self):
+        self.assertEqual(self.course_builder.set_teachers_id([1, 2]),
+                         self.course_builder)
+        self.assertEqual(self.course_builder.course.teachers, [1, 2])
+
+    def test_set_students_id(self):
+        self.assertEqual(self.course_builder.set_students_id([4, 5, 6]),
+                         self.course_builder)
+        self.assertEqual(self.course_builder.course.students, [4, 5, 6])
+
+    def test_set_address(self):
+        self.assertEqual(self.course_builder.set_address(self.address),
+                         self.course_builder)
+        self.assertEqual(self.course_builder.course.address, self.address)
+
+    def test_set_books(self):
+        self.assertEqual(self.course_builder.set_books('Gek'),
+                         self.course_builder)
+        self.assertEqual(self.course_builder.course.books, 'Gek')
+
+
+class TestCopy(unittest.TestCase):
+
+    def setUp(self):
+        self.course = CourseBuilder().set_course_id(5).\
+            set_language('English').\
+            set_address('Moscow').set_teachers_id([4]).\
+            set_students_id([5, 8]).set_books('No')
+        self.new_course = copy_course(self.course)
+
+    def test_copy_course(self):
+        self.assertNotEqual(self.course, self.new_course)
+        self.assertEqual(self.course.course.course_id,
+                         self.new_course.course.course_id)
+        self.assertEqual(str(self.course.course), str(self.new_course.course))
+
+
+class TestChangeStudent(unittest.TestCase):
+
+    def setUp(self):
+        self.address = AddressBuilder().set_city('Rom').build()
+        self.student = PersonFactory().create_person(
+            'student', 9, 'Mama',
+            'Li', 'mama@h', '9009', self.address,
+            language_level=['Italian A2', 'English B2'],
+            courses_id=[56]
+        )
+        self.change = ChangeStudent()
+
+    def test_change_name(self):
+        self.change.change_name(self.student, 'Vika')
+        self.assertEqual(self.student.name, 'Vika')
+
+    def test_add_course(self):
+        self.change.add_course(self.student, 57)
+        self.assertIn(57, self.student.courses_id)
+
+
+class TestChangeTeacher(unittest.TestCase):
+
+    def setUp(self):
+        self.address = AddressBuilder().set_city('Rom').build()
+        self.teacher = PersonFactory().create_person(
+            'teacher', 10, 'Eva',
+            'Lop', 'eva@h', '5000-56', self.address,
+            languages=['Italian', 'English'],
+            courses_id=[56],
+            diplomas=['Rom diploma'],
+            salary=5000
+        )
+        self.change = ChangeTeacher()
+
+    def test_change_name(self):
+        self.change.change_name(self.teacher, 'Adam')
+        self.assertEqual(self.teacher.name, 'Adam')
+
+    def test_add_course(self):
+        self.change.add_course(self.teacher, 57)
+        self.assertIn(57, self.teacher.courses_id)
+
+
+class TestChangeCourse(unittest.TestCase):
+
+    def setUp(self):
+        self.address = AddressBuilder().set_city('Rom').build()
+        self.course = CourseBuilder().set_language('English').set_level('B2').\
+            set_address(self.address).set_price(300).build()
+        self.change = ChangeCourse()
+
+    def test_add_teacher(self):
+        self.change.add_teacher(self.course, 11)
+        self.assertIn(11, self.course.teachers)
+
+    def test_add_student(self):
+        self.change.add_student(self.course, 11)
+        self.assertIn(11, self.course.students)
+
+
+class TestChangeFacade(unittest.TestCase):
+
+    def setUp(self):
+        self.address = AddressBuilder().set_city('Rom').build()
+        self.teacher = PersonFactory().create_person(
+            'teacher', 10, 'Eva',
+            'Lop', 'eva@h', '5000-56', self.address,
+            languages=['Italian', 'English'],
+            courses_id=[56],
+            diplomas=['Rom diploma'],
+            salary=5000
+        )
+        self.student = PersonFactory().create_person(
+            'student', 9, 'Mama',
+            'Li', 'mama@h', '9009', self.address,
+            language_level=['Italian A2', 'English B2'],
+            courses_id=[56]
+        )
+        self.course = CourseBuilder().set_course_id(45).set_language('English').\
+            set_level('B2').set_address(self.address).set_price(300).build()
+        self.course_facade = ChangeFacade()
+
+    def test_add_teacher_to_course(self):
+        self.course_facade.add_teacher_to_course(self.course, self.teacher)
+        self.assertIn(10, self.course.teachers)
+        self.assertIn(45, self.teacher.courses_id)
+
+    def test_add_student_to_course(self):
+        self.course_facade.add_student_to_course(self.course, self.student)
+        self.assertIn(9, self.course.students)
+        self.assertIn(45, self.student.courses_id)
+
 
 if __name__ == '__main__':
-    _course = Course(language='english', level='A1', price='200',
-                     teachers=['Anna'], students=['Ivan', 'Pety', 'Maria'],
-                     address='Munich', books='LLoL')
-    copy_cour = copy_course(_course)
-    print(copy_cour)
-    change_cour = Change(copy_cour, level='A2', price='400')
-    cour = change_cour.change_data()
-    print(cour)
-
-    addr = AddressBuilder().set_city('Paris').\
-        set_street('Tred').set_country('Germany').build()
-    print(addr)
+    unittest.main()
